e920fba092eb7efcef95d7953dcb8b577a6e4e99
hiep-tm <tranhiep.az2@gmail.com>
2025-07-17 02:29:47 +0700

    Remove build files

--- App.Entities/DTOs/Handbag/SearchHandbagResultDTO.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace App.Entities.DTOs.Handbag;
-
-public class SearchHandbagResultDTO
-{
-    public string BrandName { get; set; } = null!;
-    public List<HandbagResponseDTO> Handbags { get; set; } = new List<HandbagResponseDTO>();
-}
--- App.Entities/Enums/UserRole.cs
+++
@@ -1,8 +0,0 @@
-namespace App.Entities.Enums;
-public enum UserRoles
-{
-    Administrator = 1,
-    Moderator = 2,
-    Developer = 3,
-    Member =  4
-}
