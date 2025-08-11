20a571719985bc3a34b61b63cd2511fed8c4ef7c
hiep-tm <tranhiep.az2@gmail.com>
2025-08-11 13:56:47 +0700

    fea: add DTOs for bulk review, history... (reviewer suggestion)

---
+++ App.Entities/DTOs/ReviewerSuggestion/BulkReviewerSuggestionOutputDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities.DTOs.ReviewerSuggestion
+{
+    public class BulkReviewerSuggestionOutputDTO
+    {
+        public int TopicVersionId { get; set; }
+        public ReviewerSuggestionOutputDTO Suggestion { get; set; }
+    }
+}
---
+++ App.Entities/DTOs/ReviewerSuggestion/ReviewerEligibilityDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities.DTOs.ReviewerSuggestion
+{
+    public class ReviewerEligibilityDTO
+    {
+        public int ReviewerId { get; set; }
+        public int TopicVersionId { get; set; }
+        public bool IsEligible { get; set; }
+        public List<string> Reasons { get; set; }
+    }
+}
