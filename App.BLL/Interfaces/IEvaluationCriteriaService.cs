5827339cd002c76defb3dd94b90a88b6bc426f17
hiep-tm <tranhiep.az2@gmail.com>
2025-08-15 11:49:16 +0700

    fea: add interfaces, config (review, evaluation criteria)

---
+++ App.BLL/Interfaces/IEvaluationCriteriaService.cs
@@ -0,0 +1,15 @@
+﻿using App.Commons.ResponseModel;
+using App.Entities.DTOs.EvaluationCriteria;
+using App.Commons.Paging;
+
+namespace App.BLL.Interfaces;
+
+public interface IEvaluationCriteriaService
+{
+    Task<BaseResponseModel<EvaluationCriteriaResponseDTO>> CreateAsync(CreateEvaluationCriteriaDTO createDTO);
+    Task<BaseResponseModel<EvaluationCriteriaResponseDTO>> UpdateAsync(UpdateEvaluationCriteriaDTO updateDTO);
+    Task<BaseResponseModel> DeleteAsync(int id);
+    Task<BaseResponseModel<EvaluationCriteriaResponseDTO>> GetByIdAsync(int id);
+    Task<BaseResponseModel<PagingDataModel<EvaluationCriteriaResponseDTO>>> GetAllAsync(PagingModel pagingModel);
+    Task<BaseResponseModel<List<EvaluationCriteriaResponseDTO>>> GetAllActiveAsync();
+}
---
+++ App.BLL/Interfaces/IReviewService.cs
@@ -0,0 +1,16 @@
+﻿using App.Commons.ResponseModel;
+using App.Entities.DTOs.Review;
+using App.Commons.Paging;
+
+namespace App.BLL.Interfaces;
+
+public interface IReviewService
+{
+    Task<BaseResponseModel<ReviewResponseDTO>> CreateAsync(CreateReviewDTO createDTO);
+    Task<BaseResponseModel<ReviewResponseDTO>> UpdateAsync(UpdateReviewDTO updateDTO);
+    Task<BaseResponseModel> DeleteAsync(int id);
+    Task<BaseResponseModel<ReviewResponseDTO>> GetByIdAsync(int id);
+    Task<BaseResponseModel<PagingDataModel<ReviewResponseDTO>>> GetAllAsync(PagingModel pagingModel);
+    Task<BaseResponseModel<ReviewResponseDTO>> SubmitReviewAsync(int reviewId);
+    Task<BaseResponseModel<List<ReviewResponseDTO>>> GetReviewsByAssignmentAsync(int assignmentId);
+}
