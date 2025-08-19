0f94c32162c0374f0498ac388e8f15af305071d2
hiep-tm <tranhiep.az2@gmail.com>
2025-08-19 12:40:29 +0700

    fea: API login with role

--- App.Entities/DTOs/Auth/LoginDTO.cs
+++ App.Entities/DTOs/Auth/LoginDTO.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using App.Commons;
+using App.Entities.Entities.Core;
+using App.Entities.Enums;
 
 namespace App.Entities.DTOs.Auth;
 
@@ -10,4 +12,7 @@ public class LoginDTO
 
     [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = null!;
+
+    [Required(ErrorMessage = "Role is required")]
+    public SystemRoles Role { get; set; }
 }
