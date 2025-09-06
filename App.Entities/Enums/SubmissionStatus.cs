2c3ea0ebaf079c0b76b7487e559e050a7be53508
SimondaVinciii <hqthinh154@gmail.com>
2025-09-06 19:37:07 +0700

    add: note for submissionstatus

--- App.Entities/Enums/SubmissionStatus.cs
+++ App.Entities/Enums/SubmissionStatus.cs
@@ -1,10 +1,11 @@
-namespace App.Entities.Enums;
+﻿namespace App.Entities.Enums;
 
 public enum SubmissionStatus
 {
     Pending = 1,
     UnderReview = 2,
     Duplicate = 3,
+    [Obsolete("Không sử dụng chung chung thế này nữa.")]
     Completed = 4,
     RevisionRequired = 5,
     EscalatedToModerator = 6,
